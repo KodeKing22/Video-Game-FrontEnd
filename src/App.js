e4fb854a42a5a4c1319d3c0425a7294aab9e9d2a
KodeKing22 <jonkylemars@yahoo.com>
2022-07-07 16:46:37 -0500

    Created DisplayVideoGames Folder and File + Created VideoGameChart Folder and File

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+// import './App.css';
+
+function App() {
+  
+  
+  return (
+    <div>
+      <h3>Platform By Global Sales in Millions</h3>
+    </div>
+    
+  );
+}
+
+export default App;
