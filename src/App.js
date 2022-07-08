7c0efe4402e0e5d04f93942cbd159ab04ea20432
KodeKing22 <jonkylemars@yahoo.com>
2022-07-07 19:37:52 -0500

    Created Search Video Games File

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect } from 'react';
-// import './App.css';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  
+
+  const [games, setGames] = useState([]);
+
+  async function getAllVideoGames(){
+    let response = await axios.get("http://localhost:8080/all");
+    setGames(response.data);
+    console.log(response.data);
+  }
+  useEffect(() =>{
+    getAllVideoGames();
+
+  },[])
+
+  async function SearchVideoGames(getGame){
+    let response = await axios.get("http://localhost:8080/all", getGame);
+    if(response.data);
+    await getAllVideoGames();
+  }
   
   return (
     <div>
---
+++ src/Components/VideoGameChart/VideoGameChart.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+
+const VideoGameChart = () => {
+    return ({} );
+}
+ 
+export default VideoGameChart;
